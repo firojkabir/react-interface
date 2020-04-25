65e6409e7eb18b3870610e4fd8c23b6d251a1401
FirozKabir65 <firoj.kabir007@gmail.com>
2020-04-26 00:27:46 +0200

    Tracking from display

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ class App extends Component {
 		super();
 		this.state = {
 			myAppointments: [],
+			formDisplay: false,
 			lastIndex: 0
 		};
 		this.deleteAppointment = this.deleteAppointment.bind(this)
@@ -48,7 +49,9 @@ class App extends Component {
 					<div className="row">
 						<div className="col-md-12 bg-white">
 							<div className="container">
-								<AddAppointments />
+								<AddAppointments 
+									formDisplay={this.state.formDisplay}
+								/>
 								<SearchAppointments />
 								<ListAppointments appointments={this.state.myAppointments}
 								deleteAppointment={this.deleteAppointment}/>
